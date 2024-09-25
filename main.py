18e4bba2fa78dbc7bd4e9350e2b9fe118036a6b6
AJ <114190202+crustylemons@users.noreply.github.com>
2024-09-25 22:20:47 +0000

    can retrieve all json data from tracks and started developing sorting playlist logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import base64
 from requests import post, get
 import json
+import requests
 
 # Retrieves client id and secret from the .env file
 load_dotenv()
@@ -59,8 +60,22 @@ def search_for_artist(token, artist_name):
     
     return json_result[0]
 
+# Retreives all json data of playlists from a user
+def get_all_playlists(token, username):
+    url = f"https://api.spotify.com/v1/users/{username}/playlists"
+    headers = get_auth_header(token) # Proves I can access Spotify's data
 
-# Retrieve a playlist
+    # Send GET request to API
+    result = get(url, headers=headers)
+
+    # Turns json into python dictionary and puts the results in an array
+    json_result = json.loads(result.content)
+    playlists = json_result.get("items", [])
+    
+    for playlist in playlists:
+        print(playlist['name'])
+
+# Retrieve all json data of a playlist
 def get_my_playlist_id (token, playlist_name, username):
     url = f"https://api.spotify.com/v1/users/{username}/playlists"
     headers = get_auth_header(token) # Proves I can access Spotify's data
@@ -81,29 +96,102 @@ def get_my_playlist_id (token, playlist_name, username):
     for playlist in playlists:
         #print(playlist)
         if playlist['name'].lower() == playlist_name.lower():
-            print(f"{playlist['name']} - getting id: ")
+            #print(f"{playlist['name']} - getting id: {playlist['id']}")
             return playlist['id']
 
     return "There are no playlists with this name"
 
-# Retreives all playlists from a user
-def get_all_playlists(token, username):
+# Retrieve all json data of tracks from a playlist
+def get_all_tracks (token, playlist_name, username):
+    playlist_id = get_my_playlist_id(token, playlist_name, username)
+    
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    headers = get_auth_header(token) # Proves I can access Spotify's data
+
+    all_tracks = []
+
+    # while loop that gets items 100 at a time
+    while url: # While there's more songs to fetch
+        # Send GET request to API
+        result = get(url, headers=headers)
+
+        # Check if the request was successful
+        if result.status_code == 200:
+            # Turns json into python dictionary
+            json_result = json.loads(result.content)
+                
+            # Append the current batch of tracks to the all_tracks list
+            all_tracks.extend(json_result['items'])
+
+            url = json_result['next']
+
+        else:
+            print("unsuccessful retrevial")
+            break
+
+    
+    #song_names = [item['track']['name'] for item in all_tracks]
+    return all_tracks
+
+# Create a new playlist
+### DOESN'T WORK YET
+def create_playlist(token, new_playlist_name, username):
+    user_id = get_my_playlist_id()
     url = f"https://api.spotify.com/v1/users/{username}/playlists"
     headers = get_auth_header(token) # Proves I can access Spotify's data
 
-    # Send GET request to API
-    result = get(url, headers=headers)
+    # Define the request body
+    data = {
+        "name": new_playlist_name,
+        "description": f"Playlist created via API: {new_playlist_name}",
+        "public": False
+    }
 
-    # Turns json into python dictionary and puts the results in an array
-    json_result = json.loads(result.content)
-    playlists = json_result.get("items", [])
+    # Requests to make a new playlist to the API
+    response = requests.post(url, headers=headers, json=data)
+
+    # Check if the request was successful
+    if response.status_code == 201:
+        # Parse the JSON response
+        playlist = response.json()
+        print(f"Playlist '{playlist['name']}' created successfully!")
+        print(playlist['items']['name'])
+        return playlist  # Return the playlist data
+    else:
+        # Handle error responses
+        print(f"Failed to create playlist: {response.status_code} - {response.text}")
+        return None
+
+
+
+
+
+# Sorts all songs from a playlist by seperating albums into a new playlist
+def sort_playlist(token, playlist_name, username, new_playlist_name):
+    # gets all playlist tracks
+    playlist_tracks = get_all_tracks(token, playlist_name, username)
     
-    for playlist in playlists:
-        print(playlist['name'])
+    # create a new playlist called new_playlist_name
+    ####
 
+    # for each track on playlist_name
+        # add to playlist new_playlist_name
+    # for each track on playlist_name, starting on track 2, 
+        # if the previous song's album = current song's album
+            # create max_random_num = 10
+            # if playlist length is less than 10,
+                # set max_random_num to the playlist length
+            # generate a random number from 2 - max_random_num
+            # put the track at that spot in the playlist
+    
+    # print out: new_playlist_name + " has been created!"
+    
 
 # Start calling functions here :
 token = get_token()
+username = "lilyh396113"
 
 
-get_all_playlists(token, "lilyh396113")
+#get_all_playlists(token, "lilyh396113")
+#get_all_tracks(token, "Adamant_Ashley‘s Main Playlist", "lilyh396113")
+create_playlist(token, "new playlist", username)
